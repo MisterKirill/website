da6a53a42ec2f166ffd4f4711543936e52cf2082
misterkirill <misterkirill666@gmail.com>
2025-02-05 21:32:14 +0500

    fix: fix theme switch

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -7,11 +7,9 @@ import Loader from './Loader';
 
 export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
 
-  const switchTheme = () => {
-    setTheme(theme == 'dark' ? 'light' : 'dark');
-  };
+  const currentTheme = theme === 'system' ? systemTheme : theme;
 
   useEffect(() => {
     setMounted(true);
@@ -24,9 +22,9 @@ export default function ThemeSwitch() {
   return (
     <button
       className="cursor-pointer transition-[scale,background-color] p-3 ease-in-out duration-150 rounded-full text-teal-700 dark:text-teal-300 hover:bg-teal-200 dark:hover:bg-teal-800 active:scale-90"
-      onClick={switchTheme}
+      onClick={() => setTheme(currentTheme == 'dark' ? 'light' : 'dark')}
     >
-      {theme == 'dark' ? <Sun size={25} /> : <Moon size={25} />}
+      {currentTheme == 'dark' ? <Sun size={25} /> : <Moon size={25} />}
     </button>
   );
 }
